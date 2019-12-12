376067794a4fded1a456f5acd0cff8ef10082b38
vrees <viktor.rees@gmx.de>
2019-12-12 08:58:02 +0100

    Statemachine added
    (cherry picked from commit 02670f047965fe50929f8a4e2f8111921f196399)
    updated config dir

--- src/main/java/de/vrees/heatpump/domain/Authority.java
+++ src/main/java/de/vrees/heatpump/domain/Authority.java
@@ -1,7 +1,11 @@
 package de.vrees.heatpump.domain;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
@@ -10,7 +14,9 @@ import java.util.Objects;
 /**
  * An authority (a security role) used by Spring Security.
  */
-@Document(collection = "jhi_authority")
+@Entity
+@Table(name = "jhi_authority")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class Authority implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -18,6 +24,7 @@ public class Authority implements Serializable {
     @NotNull
     @Size(max = 50)
     @Id
+    @Column(length = 50)
     private String name;
 
     public String getName() {
